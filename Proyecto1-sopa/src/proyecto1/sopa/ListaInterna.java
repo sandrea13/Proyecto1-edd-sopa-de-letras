3a1363a169ec34e0a9f78432c39d07b127cd7c04
Fofo0305 <adolfo.castillo@correo.unimet.edu.ve>
2025-06-12 07:50:40 -0400

    Metodos Faltantes en la lista interna

--- Proyecto1-sopa/src/proyecto1/sopa/ListaInterna.java
+++ Proyecto1-sopa/src/proyecto1/sopa/ListaInterna.java
@@ -41,4 +41,37 @@ public class ListaInterna {
     }
     
     
+    public void Agg(Nodo n1, Nodo n2) {
+        Arco nuevoArco = new Arco(n1, n2);
+        if (this.isEmpty()) {
+            this.setUltimo(nuevoArco);
+            this.setCabeza(nuevoArco);
+        } else {
+            this.Ultimo.siguiente = nuevoArco;
+            this.setUltimo(nuevoArco);
+        }
+    }
+    
+    
+    public boolean Buscar(Nodo uno) {
+        Arco aux = Cabeza;
+        while (aux != null && (aux.nLetra != uno && aux.nLetra2 != uno)) {
+            aux = aux.siguiente;
+        }
+        return aux != null;
+    }
+    
+    
+    public void Imprimir() {
+        Arco aux = Cabeza;
+        if (aux != null) {
+            System.out.print(aux.nLetra.getLetras());
+            while (aux != null) {
+                System.out.print("-" + aux.nLetra2.getLetras());
+                aux = aux.siguiente;
+            }
+            System.out.println("");
+        }
+    }
+    
 }
