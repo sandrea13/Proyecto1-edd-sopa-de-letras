3f5f69c28da8ef0450aa53c6aa92950cffb7c2ad
Fofo0305 <adolfo.castillo@correo.unimet.edu.ve>
2025-06-15 21:14:30 -0400

    Inicio de las funciones de BFS y DFS, con mensaje de tiempo

--- Proyecto1-sopa/src/proyecto1/sopa/Ventana1.java
+++ Proyecto1-sopa/src/proyecto1/sopa/Ventana1.java
@@ -125,11 +125,29 @@ public class Ventana1 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        long startTime = System.currentTimeMillis();
+        
+        for (int i = 0; i < v1.palabras.length; i++) {
+            String palabra = v1.palabras[i];
+        }
+        
+        long endTime = System.currentTimeMillis();
+        long tiempoDeEjecucion = endTime - startTime;
+
+        JOptionPane.showMessageDialog(null, "Tiempo de ejecución: " + tiempoDeEjecucion + " milisegundos");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        long startTime = System.currentTimeMillis();
+        
+        for (int i = 0; i < v1.palabras.length; i++) {
+            String palabra = v1.palabras[i];
+        }
+        
+        long endTime = System.currentTimeMillis();
+        long tiempoDeEjecucion = endTime - startTime;
+
+        JOptionPane.showMessageDialog(null, "Tiempo de ejecución: " + tiempoDeEjecucion + " milisegundos");
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
